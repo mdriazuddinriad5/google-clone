6b97d2916c3671d650bfc27550de5fca3891d521
mdriazuddinriad5 <mdriazuddinriad5@gmail.com>
2023-12-17 12:01:16 +0600

    Navbar setup

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,15 @@
-
+import { IoApps } from "react-icons/io5";
 
 const Navbar = () => {
     return (
         <div>
-            This is Navbar
+            <div className="flex items-center justify-end border-x-2  border-y-2 bg-base-100">
+                <ul className="menu menu-horizontal px-1">
+                    <li><a>Gmail</a></li>
+                    <li><a>Images</a></li>
+                    <li><a><IoApps className="mt-1" /></a></li>
+                </ul>
+            </div>
         </div>
     );
 };
